705a00cd5c17abf971c78f3a5943d498bfc04844
A <abbasgure@gmail.com>
2024-10-16 12:05:09 +0100

    item entity created, will now create the contract for item and employee

--- Models/Entities/Item.cs
+++ Models/Entities/Item.cs
@@ -1,9 +1,27 @@
-﻿namespace Althaus_Warehouse.Models.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Althaus_Warehouse.Models.Entities
 {
+
+    /// <summary>
+    /// Entity class representing a warehouse item
+    /// </summary>
     public class Item
     {
-
+        /// <summary>
+        /// Non args constructor for Item
+        /// </summary>
         public Item() { }
+
+        /// <summary>
+        /// Constructor for an item
+        /// </summary>
+        /// <param name="id">Item Id</param>
+        /// <param name="name">Item name</param>
+        /// <param name="description">Brief description of item</param>
+        /// <param name="quantity">Current quantity of item</param>
+        /// <param name="price">Item Price</param>
         public Item(int id, string name, string description, int quantity, int price)
         {
             Id = id;
@@ -13,11 +31,35 @@
             Price = price;
         }
 
+        /// <summary>
+        /// Gets or sets the unique identifier for the item
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the item
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets a brief description of the item
+        /// </summary>
+        [MaxLength(200)]
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current quantity of the item in stock
+        /// </summary>
         public int Quantity { get; set; }
 
+        /// <summary>
+        /// Gets or sets the price of the item
+        /// </summary>
+        [Required]
         public int Price { get; set; }
     }
 }
