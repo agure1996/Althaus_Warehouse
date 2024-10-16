bc1c3d4111c7e5765d7c3890bfcea60984d6e9e8
A <abbasgure@gmail.com>
2024-10-16 12:00:00 +0100

    item entity created

---
+++ Models/Entities/Item.cs
@@ -0,0 +1,23 @@
+﻿namespace Althaus_Warehouse.Models.Entities
+{
+    public class Item
+    {
+
+        public Item() { }
+        public Item(int id, string name, string description, int quantity, int price)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Quantity = quantity;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+
+        public int Price { get; set; }
+    }
+}
